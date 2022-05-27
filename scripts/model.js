92c32cb57f8144af21e23ae489c3268b8b3fc5a1
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-05-27 14:54:07 +0200

    Add and Get Model

--- scripts/model.js
+++ scripts/model.js
@@ -11,4 +11,53 @@ class Model {
         this.user = []
         this.chrono = []
     }
+
+    /**
+    * Crea un objeto de la clase ImageAdjust
+    * @param {String} id Id con el que será creado el objeto de la clase ImageAdjust
+    */
+    addImg(id){
+        this.image.push(new ImageAdjust(id))
+    }
+
+    /**
+    * Devuelve el array con los objetos de las imagenes creadas
+    * @return {Array} Array con los objetos de la clase Imagenes
+    */
+    getImg(id) {
+        return this.image.find(_image => _image.id === id)
+    }
+
+    /**
+    * Devuelve el array con los objetos de los chrono creados
+    * @return {Array} Array con los objetos de la clase Chrono
+    */
+    getChrono(id) {
+        return this.chrono.find(_chrono => _chrono.id === id)
+    }
+
+    /**
+    * Crea un objeto de la clase Chrono
+    * @param {String} id Id con el que será creado el objeto de la clase Chrono
+    */
+    addChrono(id){
+        this.chrono.push(new Chrono(id))
+    }
+
+    /**
+    * Devuelve el array con los objetos de los User creados
+    * @returns {Array} Array con los objetos de la clase User
+    */
+    getUser(id) {
+        return this.user.find(_user => _user.id === id)
+    }
+
+    /**
+    * Crea un objeto de la clase User
+    * @param {String} id Id con el que será creado el objeto de la clase User
+    */
+    addUser(id){
+        this.user.push(new User(id))
+    }
+
 }
