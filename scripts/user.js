58272ce450bf3249b4135a9f9c6769d78c5d2c5b
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-05-27 14:40:41 +0200

    Getters and Setters User

--- scripts/user.js
+++ scripts/user.js
@@ -13,4 +13,52 @@ class User{
         this.userName;
         this.id = id
     }
+
+    /**
+        * Define la cantidad de clicks del jugador que ha realizado para encontrar el tesoro
+        * @param {Number} clicks
+        */
+     setClicks(clicks) {
+        this.clicks = clicks
+    }
+
+    /**
+    * Devluelve la cantidad de clicks del jugador
+    * @returns {Number} 
+    */
+    getClicks() {
+        return this.numclicks 
+    }   
+
+    /**
+    * Define la cantidad de tiempo que el jugador ha tardado en encontrar el tesoro
+    * @param {Number | String} time
+    */
+    setTime(time) {
+        this.time = time
+    }
+
+    /**
+    * Devluelve el timepo que ha tardado el jugador
+    * @returns {Number | String} 
+    */
+    getTime() {
+        return this.time
+    }
+
+    /**
+    * Define el nombre del jugador
+    * @param {String} userName
+    */
+    setUserName(userName) {
+        this.userName = userName
+    }
+
+    /**
+    * Devluelve el nombre del jugador
+    * @returns {String} 
+    */
+    getUserName() {
+        return this.userName
+    }
 }
