2c3be2ddf7dede774a63a00951f2bdccdc369189
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-05-27 14:44:51 +0200

    Getters and Setters Img

--- scripts/image.js
+++ scripts/image.js
@@ -11,4 +11,37 @@ class ImageAdjust {
         this.HEIGH = 0
         this.id = id
     }
+
+    /**
+    * Define la ancho de la imagen
+    * @param {Number} WIDTH
+    */
+    setWidth(WIDTH) {
+        this.WIDTH = WIDTH
+    }
+        
+    /**
+    * Devluelve la ancho de la imagen
+    * @returns {Number} 
+    */
+    getWidth() {
+        return this.WIDTH
+    }
+    
+    /**
+    * Define el alto de la imagen
+    * @param {Number} HEIGH
+    */
+    setHeigh(HEIGH) {
+        this.HEIGH = HEIGH
+    }
+        
+    /**
+    * Devluelve el alto de la imagen
+    * @returns {Number} 
+    */
+    getHeigh() {
+        return this.HEIGH
+    }
+    
 }
