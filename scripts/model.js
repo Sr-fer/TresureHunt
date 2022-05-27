64b70e0a7648a7376325bdf8b36ebbb6e3a06064
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-05-27 14:53:16 +0200

    Class Model

---
+++ scripts/model.js
@@ -0,0 +1,14 @@
+class Model {
+
+    /**
+    * @type {Array} Array que guarda los objetos creados de la clase imagen
+    * @type {Array} Array que guarda los objetos creado de la clase user
+    * @type {Array} Array que guarda los objetos creado de la clase chrono
+    */
+
+    constructor() {
+        this.image = []
+        this.user = []
+        this.chrono = []
+    }
+}
