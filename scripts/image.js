0ea981f47e98adb0618a362514e6a00892816253
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-05-27 14:43:00 +0200

    Img Class

---
+++ scripts/image.js
@@ -0,0 +1,14 @@
+class ImageAdjust {
+
+    /**
+    * @type {Number} Altura Imagen
+    * @type {Number} Ancho Imagen
+    * @param {String} id Id con el que es referenciada cada clase del objeto
+    */
+
+    constructor(id) {
+        this.WIDTH = 0
+        this.HEIGH = 0
+        this.id = id
+    }
+}
