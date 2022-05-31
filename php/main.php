7be2695f5ce75c971b8251376c56809321f1784a
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-05-31 19:51:13 +0200

    Llamada BD PHP

---
+++ php/main.php
@@ -0,0 +1,35 @@
+<?php
+class User{
+    public $Name;
+    public $Time;
+    public $Clicks;
+        function __construct($Name,$Time,$Clicks){
+            $this->Name=$Name;
+            $this->Time=$Time;
+            $this->Clicks=$Clicks;
+        }
+}
+
+    $serverName = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "ranking";
+    $conection = new mysqli($serverName,$user,$password,$database);
+
+        if($conection->connect_error){
+            die("Failed: ". $conection->connect_error);
+        }   
+
+    $consult = "SELECT * FROM ranking";
+    $result = $conection->query($consult);
+    $users = array();
+        if($result->num_rows>0){
+            while($row = $result->fetch_assoc()){
+            $aux_user = new User($row["name"],$row["time"],$row["clicks"]);
+            array_push($users,$aux_user);
+        }
+        }else{
+            print"Error: ".$conection->error;
+        }
+        echo json_encode($users);
+?>
